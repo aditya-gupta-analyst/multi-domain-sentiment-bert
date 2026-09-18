4e5b5ee132ec4d3bb8d14f7e94fa6fa0c8cfd8da
ADITYA GUPTA <reach.adityagupta.in@gmail.com>
2026-09-18 08:41:36 +0530

    Add import statement for streamlit module
    The code attempts to import the 'streamlit' module, which is not installed.

---
+++ app.py
@@ -0,0 +1,19 @@
+ ---------------------------------------------------------------------------
+ModuleNotFoundError                       Traceback (most recent call last)
+/tmp/ipykernel_773/1153541147.py in <cell line: 0>()
+----> 1 import streamlit as st
+      2 import torch
+      3 from transformers import BertForSequenceClassification, AutoTokenizer
+      4 
+      5 st.set_page_config(page_title="FinCard AI Sentiment Dashboard", layout="wide")
+
+ModuleNotFoundError: No module named 'streamlit'
+
+---------------------------------------------------------------------------
+NOTE: If your import is failing due to a missing package, you can
+manually install dependencies using either !pip or !apt.
+
+To view examples of installing some common dependencies, click the
+"Open Examples" button below.
+---------------------------------------------------------------------------
+Open Examples
